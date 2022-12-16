6bb41c79ea2dffefae88cfbe7eb5ca0b87125c65
Le Ho <leho.tdc2019@gmail.com>
2022-12-16 09:12:12 +0700

    slider

---
+++ web-hsc/assets/js/slide.js
@@ -0,0 +1,7 @@
+$(".slider").slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  prevArrow: '<button class="slick-prev"> < </button>',
+  nextArrow: '<button class="slick-next"> > </button>',
+});
