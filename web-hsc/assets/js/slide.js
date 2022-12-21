8cd7316761d764c0a3551e8cc947c069f04c6540
Le Ho <leho.tdc2019@gmail.com>
2022-12-21 11:50:11 +0700

    slick syncing

--- web-hsc/assets/js/slide.js
+++ web-hsc/assets/js/slide.js
@@ -25,3 +25,44 @@ $(".list-news").slick({
   autoplaySpeed: 3000,
   autoplay: true,
 });
+
+var numSlick = 0;
+$(".slider-for").each(function () {
+  numSlick++;
+  $(this)
+    .addClass("slider-" + numSlick)
+    .slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      prevArrow:
+        '<button class="slick-prev slick-slider-arrow"> <i class="fa-solid fa-caret-left"></i> </button>',
+      nextArrow:
+        '<button class="slick-next slick-slider-arrow"> <i class="fa-solid fa-caret-right"></i> </button>',
+      fade: true,
+      asNavFor: ".slider-nav.slider-" + numSlick,
+    });
+});
+numSlick = 0;
+$(".slider-nav").each(function () {
+  numSlick++;
+  $(this)
+    .addClass("slider-" + numSlick)
+    .slick({
+      vertical: false,
+      centerMode: true,
+      slidesToShow: 5,
+      infinite: true,
+      slidesToScroll: 1,
+      asNavFor: ".slider-for.slider-" + numSlick,
+
+      focusOnSelect: true,
+      // responsive: [
+      //   {
+      //     breakpoint: 800,
+      //     settings: {
+      //       slidesToShow: 3,
+      //     },
+      //   },
+      // ],
+    });
+});
