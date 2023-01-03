6e20abdf0ec3b98d57f1b5d971e4ccd547547733
Le Ho <leho.tdc2019@gmail.com>
2023-01-03 20:43:54 +0700

    create first

---
+++ demo-xaydung-trungtin/assets/js/slick.js
@@ -0,0 +1,11 @@
+$(".slideshow").slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  prevArrow:
+    '<button class="slick-prev"> <i class="fa-solid fa-chevron-left"></i> </button>',
+  nextArrow:
+    '<button class="slick-next"> <i class="fa-solid fa-chevron-right"></i> </button>',
+  autoplay: true,
+  autoplaySpeed: 3000,
+});
