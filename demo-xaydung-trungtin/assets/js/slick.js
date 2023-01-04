7794e4cf3211f9d2b7cae271b6f1e67b30e1d2bd
Le Ho <leho.tdc2019@gmail.com>
2023-01-04 16:46:11 +0700

    update layout

--- demo-xaydung-trungtin/assets/js/slick.js
+++ demo-xaydung-trungtin/assets/js/slick.js
@@ -9,4 +9,40 @@ $(".slideshow").slick({
   autoplay: true,
   autoplaySpeed: 3000,
 });
-
+$(".slideshow-info").slick({
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  prevArrow:
+    '<button class="slick-prev"> <i class="fa-solid fa-chevron-left"></i> </button>',
+  nextArrow:
+    '<button class="slick-next"> <i class="fa-solid fa-chevron-right"></i> </button>',
+  autoplay: true,
+  autoplaySpeed: 3000,
+});
+$(".slideshow-design").slick({
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  arrows: false,
+  // prevArrow:
+  //   '<button class="slick-prev"> <i class="fa-solid fa-chevron-left"></i> </button>',
+  // nextArrow:
+  //   '<button class="slick-next"> <i class="fa-solid fa-chevron-right"></i> </button>',
+  autoplay: true,
+  autoplaySpeed: 3000,
+  vertical: true,
+  touchMove: true,
+});
+$(".construction-project-slideshow").slick({
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  prevArrow:
+    '<button class="slick-prev"> <i class="fa-solid fa-chevron-left"></i> </button>',
+  nextArrow:
+    '<button class="slick-next"> <i class="fa-solid fa-chevron-right"></i> </button>',
+  // autoplay: true,
+  // autoplaySpeed: 3000,
+  
+});
